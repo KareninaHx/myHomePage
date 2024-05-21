a1233b6f57430485c7361a87de4e701b4c696054
wuqi <1379121780@qq.com>
2024-05-21 18:35:43 +0800

    完成项目创建；完成背景图和首页导航栏；TODO：服务页面和关于我基础页面编写

---
+++ js/index/index.js
@@ -0,0 +1,16 @@
+/**
+ * 页面主要脚本文件  入口文件
+ */
+
+
+function log(text){
+  console.log("🚀 ~ log ~ text:", text)
+}
+// 获取页面高度
+var pHeight = window.innerHeight
+var pWidth = window.innerWidth
+log(pHeight)
+log(pWidth)
+
+
+
